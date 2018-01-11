aafc3ef11bd631f0f03f34ea6f38661f514f5cb2
s-light <github@s-light.eu>
2018-01-11 17:11:22 +0100

    some code comment style updates

--- DeviceIdentity.h
+++ DeviceIdentity.h
@@ -1,47 +1,41 @@
-/************	**************************************************************************************
-		
-	DeviceIdentity.h
-		Identity structures
-		
-		for more information see *.ino
-		
-**************************************************************************************************/
-/**************************************************************************************************
-***************************************************************************************************/
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//  DeviceIdentity.h
+//  Identity structures
+//
+//  for more information see *.ino
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /** Serial.print to Flash: Notepad++ Replace RegEx
-	Find what:		Serial.print(.*)\("(.*)"\);
-	Replace with:	Serial.print\1(F("\2"));
+  Find what:		Serial.print(.*)\("(.*)"\);
+  Replace with:	Serial.print\1(F("\2"));
 **/
 
 
-/**************************************************************************************************/
-/** includes                                                                                     **/
-/**************************************************************************************************/
+#ifndef DEVICEIDENTITY_H_
+#define DEVICEIDENTITY_H_
+
+// includes
+
 // core arduino
 #include <Arduino.h>
 
-/**************************************************************************************************/
-/** type and struct definition                                                                   **/
-/**************************************************************************************************/
+// type and struct definition
 
-/**/
 // Individual Hardware Addresses
 struct tDeviceHardware {
-	uint16_t ID;
-	uint8_t radio_Frequency;
+  uint16_t ID;
+  uint8_t radio_Frequency;
 };
 
 // Device Configuration
 struct tDeviceConfig {
-	uint8_t bBallID;
-	uint8_t bNetworkID;
-	uint8_t bMasterID;
-	uint8_t bBallStartAddress;
-	uint16_t uiFadeTime;
+  uint8_t bBallID;
+  uint8_t bNetworkID;
+  uint8_t bMasterID;
+  uint8_t bBallStartAddress;
+  uint16_t uiFadeTime;
 };
-/**/
 
-/************************************************/
-/** END                                        **/
-/************************************************/
+#endif  // DEVICEIDENTITY_H_
